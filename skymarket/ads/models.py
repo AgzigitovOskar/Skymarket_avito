8d5cc7829670330ae6b7326845f797228d819b58
Oskar Agzigitov <110802094+AgzigitovOskar@users.noreply.github.com>
2023-03-06 23:29:41 +0300

    models

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,29 @@
-from django.conf import settings
 from django.db import models
 
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    # objects = None
+    title = models.CharField(max_length=100, verbose_name='Название товара')
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='ad_images', null=True, blank=True)
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+
+    # class Meta:
+    #     verbose_name = 'Отзыв'
+    #     verbose_name_plural = 'Отзывы'
+    #     ordering = ['-created_at']
+    #
+    # def __str__(self):
+    #     return self.text
+
+
+
